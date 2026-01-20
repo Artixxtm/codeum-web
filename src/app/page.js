de942bfec1faf5319da5f6289351b2594f1b6b1c
Artixx25 <officalwoolf@gmail.com>
2026-01-20 23:53:50 +0100

    upload clean (fixed paths)

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
-import Nav from "./components/Nav";
-import Header from "../sections/Header";
-import Projects from "../sections/Projects";
-import Sky from "../sections/Sky";
+import Nav from "@/components/Nav";
+import Header from "@/sections/Header";
+import Projects from "@/sections/Projects";
+import Sky from "@/sections/Sky";
 
 export default function Home() {
   return (
